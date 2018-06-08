fd7caeaf20cc42fd4e0361061127466ef68197be
maxwellharon <maxwellharon@gmail.com>
2018-06-08 15:15:47 +0300

    Created test

---
+++ userdata.py
@@ -0,0 +1,16 @@
+class Userdata:
+    """
+    A class that generates user  information
+    """
+    userdata = []  #Userdata information
+
+    def __init__(self,first_name,last_name,email,number):
+        self.first_name= first_name
+        self.last_name=last_name
+        self.email=email
+        self.number=number
+
+    def _userdata(self):
+        """
+        """
+        Userdata:userdata.append(self)

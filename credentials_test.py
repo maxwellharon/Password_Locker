17aae6db211fc529498778bffdb2714d3107def1
maxwellharon <maxwellharon@gmail.com>
2018-06-09 21:54:07 +0300

    Check whether objects have been initialized properly

--- credentials_test.py
+++ credentials_test.py
@@ -15,10 +15,20 @@ class TestCredentials(unittest.TestCase):
         set up method to before each test case.
         '''
 
-        self.new_credentials = Credentials("IG", "_thrills", "jkodhe@gmail.com", "password")
+        self.new_credentials = Credentials("Max", "_Max", "maxwell_haron@icloud.com", "maxisawesome")
 
     def tearDown(self):
         '''
         Method that does clean up after each test case has run
         '''
         Credentials.credentials_list = []
+
+
+    def test_credentials_init(self):
+        '''
+        Created a test that tests if the ibject has been initialized properly
+        '''
+        self.assertEqual(self.new_credentials.account_name, "Max")
+        self.assertEqual(self.new_credentials.user_name, "_Max")
+        self.assertEqual(self.new_credentials.email, "maxwell_haron@icloud.com")
+        self.assertEqual(self.new_credentials.password, "maxisawesome")

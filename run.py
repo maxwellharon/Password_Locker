8344be66b852aa94cbfdd47839aa09847d5b66e8
maxwellharon <maxwellharon@gmail.com>
2018-06-09 21:23:47 +0300

    Created class credentials

---
+++ run.py
@@ -0,0 +1,11 @@
+import pyperclip
+import random
+
+class Credentials:
+    '''
+    This is a class that generates the credentials of the user in question
+    '''
+    credentials_list = []
+    user_password_list = []
+
+    

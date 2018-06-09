bf4531d227b01f792b9b181a6ebab5e353c405f4
maxwellharon <maxwellharon@gmail.com>
2018-06-09 21:15:33 +0300

    Users are now able to save their input

--- user.py
+++ user.py
@@ -2,9 +2,19 @@ class User:
     '''
     This is a class that generates new instances for the user in question
     '''
+    user_list = []
+
     def __init__(self,login_name,password):
         '''
         This method defines properties for our user object
         '''
         self.login_name = login_name
         self.password = password
+
+
+    def save_user(self):
+        '''
+        This is a method which allows the user to save input
+        '''
+
+        User.user_list.append(self)
